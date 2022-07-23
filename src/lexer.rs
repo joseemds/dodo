37f06153949e9000f43f020d4b11d7b0ab180273
José Eduardo Monteiro <mds.josee@gmail.com>
2022-07-23 16:42:02 -0300

    feat: create dumb repl function

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,16 @@
 use std::fs::read_to_string;
+use std::io::stdin;
 
 pub fn run_file(filename: &str) {
-    let file = read_to_string(filename);
-    match file {
-      Ok(content) => println!("file contents: {}", content),
-      Err(e) => panic!("{}", e),
+    let file = read_to_string(filename).unwrap();
+    println!("file content {}", file)
+}
+
+pub fn run_repl() -> (){
+    loop {
+        println!(">");
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+        println!("{}", input)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use dodo::lexer;
 fn main() {
     let args : Vec<String> = env::args().collect();
     match args.as_slice() {
-      [] => println!("usage: dodo [file]"),
+      [_bin_path] => lexer::run_repl(),
       [_, file] => lexer::run_file(file),
       _ => println!("usage: dodo [file]"),
     }
